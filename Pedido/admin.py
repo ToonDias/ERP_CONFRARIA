1d9d6762c6328e85c75d5a788106252452757b5c
aylton <ayltondias703@gmail.com>
2025-03-12 11:01:25 -0300

    Admin Pedido

--- Pedido/admin.py
+++ Pedido/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pedido
+from .models import ItemPedido
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['codigo','cliente__nome','data_cadastro','data_entrega']
+    search_fields = ['codigo']
+    actions = None
+
+admin.site.register(Pedido, PedidoAdmin)
+
+
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = ['pedido__codigo','lote__produto__descricao','local__descricao','quantidade']
+    search_fields = ['pedido__codigo','lote__produto__descricao','local__descricao']
+    actions = None
+
+admin.site.register(ItemPedido, ItemPedidoAdmin)
