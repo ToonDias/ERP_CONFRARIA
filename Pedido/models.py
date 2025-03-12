bcbacc6bd53bc5ced8bec6a10727c828f09204a0
aylton <ayltondias703@gmail.com>
2025-03-12 10:53:31 -0300

    Models pedido

--- Pedido/models.py
+++ Pedido/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from Cliente.models import Cliente
+from Estoque.models import Lote, Local
 
 # Create your models here.
+
+class Pedido(models.Model):
+    codigo = models.CharField(max_length=20)
+    detalhes = models.TextField(max_length=254)
+    data_cadastro = models.DateField('Data de cadastro', auto_now=True)
+    data_entrega = models.DateField('Data de entrega')
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    def __str__(self):
+        return f'{self.codigo} - {self.cliente.nome}'
+
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
+    lote = models.ForeignKey(Lote, on_delete=models.PROTECT)
+    local = models.ForeignKey(Local, on_delete=models.PROTECT)
+    quantidade = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Itens de pedido'
+        verbose_name_plural = 'Itens de pedidos'
+    
+    def __str__(self):
+        return f'Lote: {self.lote.codigo} - Produto: {self.lote.produto.descricao} - Quantidade: {self.quantidade}'
+
