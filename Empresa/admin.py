5b58c1166dbd0a6f2f3a164a5096695174b382db
aylton <ayltondias703@gmail.com>
2025-03-11 23:26:26 -0300

    Models de empresa completo

--- Empresa/admin.py
+++ Empresa/admin.py
@@ -2,4 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Empresa
+from .models import Empresaendereco
+from .models import Empresacontato
 
+class EmpresacontatoAdmin(admin.ModelAdmin):
+    list_display = ['tipo_contato','contato', 'empresa']
+    search_fields = ['contato']
+    actions = None
+
+admin.site.register(Empresacontato, EmpresacontatoAdmin)
+
+class EmpresaenderecoAdim(admin.ModelAdmin):
+    list_display = ['cep', 'endereco', 'numero', 'bairro', 'cidade', 'uf']
+    search_fields = ['endereco', 'numero', 'bairro', 'cidade', 'uf']
+    actions = None
+
+admin.site.register(Empresaendereco, EmpresaenderecoAdim)
+
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ['razao_social','cnpj']
+    search_fields = ['razao_social','nome_fantasia','cnpj', 'responsavel', 'responsavel_cpf']
+    actions = None
+
+admin.site.register(Empresa, EmpresaAdmin)
--- Empresa/models.py
+++ Empresa/models.py
@@ -11,6 +11,7 @@ class Empresa(models.Model):
     responsavel_data_nasc = models.DateField('Data de nascimento')
     contato = models.CharField(max_length=20)
     email = models.EmailField('E-mail', max_length=50)
+    ativa = models.BooleanField('Ativa?',default=True)
 
     class Meta:
         verbose_name = 'Empresa'
@@ -18,3 +19,48 @@ class Empresa(models.Model):
 
     def __str__(self):
         return f'{self.razao_social}({self.cnpj})'
+    
+
+class Empresacontato(models.Model):
+    contato = models.CharField(max_length=20)
+    obs = models.TextField('Observações', max_length=254, blank=True)
+    data_cadastro = models.DateField('Data de cadastro', auto_now_add=True)
+    data_atualizacao = models.DateField('Data de atualização', auto_now=True)
+    tipo_contato = models.CharField('Tipo de contato', max_length=2,
+                                    choices=[
+                                        ('TR','Telefone residencial'),
+                                        ('TC','Telefone comercial'),
+                                        ('CP','Celular pessoal'),
+                                        ('CC','Celular comercial'),
+                                        ('WP','WhatsApp'),
+                                        ('IT','Instagram'),
+                                        ('FB','Facebook'),
+                                        ('OT','Outros')
+                                        ])
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        verbose_name = 'Empresa contato'
+        verbose_name_plural = 'Empresa contatos'
+
+    def __str__(self):
+        return f'{self.cliente.nome} - {self.tipo_contato}'
+
+class Empresaendereco(models.Model):
+    cep = models.CharField('CEP',max_length=9)
+    endereco = models.CharField('Endereço', max_length=100)
+    numero = models.IntegerField('Número')
+    bairro = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=20)
+    uf = models.CharField('UF', max_length=2)
+    complemento = models.TextField(max_length=254, blank=True)
+    ponto_referencia = models.TextField(max_length=254, blank=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
+    class Meta: 
+        verbose_name = 'Empresa endereço'
+        verbose_name_plural = 'Empresa endereços'
+
+    def __str__(self):
+        return f'{self.endereco} - {self.numero} - {self.bairro} - {self.cidade.upper()}/{self.uf.upper()}'
+
