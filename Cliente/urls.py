5d5fc0b7fc9e463eb6823d076e806f0d7c6d96fb
aylton <ayltondias703@gmail.com>
2025-03-19 23:48:43 -0300

    fim dos ajustes do dia

--- Cliente/urls.py
+++ Cliente/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('clientes/', views.ListarClientes, name='listar_clientes'),
     path('consultar/', views.ConsultarCliente, name='consultar_clientes'),
-    path('cadastrar/pessoa-fisica/', views.CadastrarPessoaFisica, name='cadastrar_pessoa_fisica'),
-    path('cadastrar/pessoa-juridica/', views.CadastrarPessoaJuridica, name='cadastrar_pessoa_juridica'),
+    path('cadastrar/pessoa-fisica', views.CadastrarPessoaFisica, name='cadastrar_pessoa_fisica'),
+    path('cadastrar/pessoa-juridica', views.CadastrarPessoaJuridica, name='cadastrar_pessoa_juridica'),
     path('cadastrar/pessoa-fisica-estrangerida', views.CadastrarPessoaFisicaEstrangeira, name='cadastrar_pessoa_fisica_estrangeira'),
     path('cadastrar/pessoa-juridica-estrangeira', views.CadastrarPessoaJuridicaEstrangeira, name='cadastrar_pessoa_juridica_estrangeira')
 ]
