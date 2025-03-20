8f22d5199c9be9f2d4c78f0d582dafda45f4fc4e
aylton <ayltondias703@gmail.com>
2025-03-19 23:30:12 -0300

    templates cliente

--- Cliente/urls.py
+++ Cliente/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clientes/', views.ListarClientes, name='ListarClientes'),
-    path('consultar', views.ConsultarCliente, name='ConsultarCliente')
+    path('clientes/', views.ListarClientes, name='listar_clientes'),
+    path('consultar/', views.ConsultarCliente, name='consultar_clientes'),
+    path('cadastrar/pessoa-fisica/', views.CadastrarPessoaFisica, name='cadastrar_pessoa_fisica'),
+    path('cadastrar/pessoa-juridica/', views.CadastrarPessoaJuridica, name='cadastrar_pessoa_juridica'),
+    path('cadastrar/pessoa-fisica-estrangerida', views.CadastrarPessoaFisicaEstrangeira, name='cadastrar_pessoa_fisica_estrangeira'),
+    path('cadastrar/pessoa-juridica-estrangeira', views.CadastrarPessoaJuridicaEstrangeira, name='cadastrar_pessoa_juridica_estrangeira')
 ]
--- Cliente/views.py
+++ Cliente/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def ConsultarCliente(request):
-    return render(request,'cliente/consultar_cliente.html', context={})
+    return render(request,'cliente/consultar_clientes.html', context={})
 
 def ListarClientes(request):
 
@@ -15,4 +15,16 @@ def ListarClientes(request):
         'clientes': clientes
     }
 
-    return render(request, 'cliente/listar_cliente.html', context)
+    return render(request, 'cliente/listar_clientes.html', context={})
+
+def CadastrarPessoaFisica(request):
+    return render(request, 'cliente/pessoa_fisica.html', context={})
+
+def CadastrarPessoaJuridica(request):
+    return render(request, 'cliente/pessoa_juridica.html', context={})
+
+def CadastrarPessoaFisicaEstrangeira(request):
+    return render(request, 'cliente/pessoa_fisica_e.html', context={})
+
+def CadastrarPessoaJuridicaEstrangeira(request):
+    return render(request, 'cliente/pessoa_juridica_e.html', context={})
