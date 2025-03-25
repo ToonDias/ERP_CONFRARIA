40f6e048f98bcaba8663d967a4bc1c63dcdea9cd
aylton <ayltondias703@gmail.com>
2025-03-24 23:20:06 -0300

    templates de categorias - list

--- Estoque/urls.py
+++ Estoque/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categoria/listar', views.ListarCategirias.as_view(), name='listar_categorias'),
-    path('cadastrar/categoria', views.CadastrarCategoria, name='cadastrar_categoria'),
+    #urls categoria
+    path('categoria/', views.CategoriaListView.as_view(), name='listar_categorias'),
+    path('categoria/adicionar/', views.CategoriaCreateView.as_view(), name='nova_categoria'),
+    path('categoria/atualizar/<int:pk>', views.CategoriaUpdateView.as_view(), name='editar_categoria'),
+    path('categoria/detalhes/<int:pk>', views.CategoriaDetailView.as_view(), name='detalhes_categoria'),
+    path('categoria/deletar/<int:pk>', views.CategoriaDeleteView.as_view(), name='deletar_categoria'),
+    #urls outros
     path('cadastrar/fornecedor', views.CadastrarFornecedor, name='cadastrar_fornecedor'),
     path('cadastrar/unidade', views.CadastrarUnidade, name='cadastrar_unidade'),
     path('cadastrar/fabricante', views.CadastrarFabricante, name='cadastrar_fabricante'),
--- Estoque/views.py
+++ Estoque/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import DeleteView
+
+from .forms import FormCategoria
 
 # Create your views here.
 from .models import Categoria
 from .models import Produto
 
-class ListarCategirias(ListView):
+
+class CategoriaListView(ListView):
     model = Categoria
-    template_name = 'estoque/listar_categorias.html'
+    template_name = 'estoque/categoria/list.html'
     context_object_name = 'categorias'
+    
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    template_name = 'estoque/categoria/create.html'
+    form_cass = FormCategoria
+    template_name = 'estoque/categoria/list.html'
+    success_url = reverse_lazy('listar_categorias')
+
+class CategoriaUpdateView(UpdateView):
+    model = Categoria
+    template_name = 'estoque/categoria/update.html'
+    form_cass = FormCategoria
+    success_url = reverse_lazy('listar_categorias')
+
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    template_name = 'estoque/categoria/delete.html'
+    success_url = reverse_lazy('listar_categorias')
+
+class CategoriaDetailView(DeleteView):
+    model = Categoria
+    template_name = 'estoque/categoria/detail.html'
+    context_object_name = 'categoria'
+
+
 
 class CadastrarProduto(ListView):
     model = Produto
